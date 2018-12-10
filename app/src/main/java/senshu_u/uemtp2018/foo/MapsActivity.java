99051de095a38ffeedea69860f5046ffab1359fa
S-zebra <shimashimazebra13@gmail.com>
2018-12-11 01:12:26 +0900

    Sydney削除

--- app/src/main/java/senshu_u/uemtp2018/foo/MapsActivity.java
+++ app/src/main/java/senshu_u/uemtp2018/foo/MapsActivity.java
@@ -5,12 +5,10 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
 
 import java.util.List;
@@ -44,17 +42,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   public void onMapReady(GoogleMap googleMap) {
     mMap = googleMap;
   
-    // Add a marker in Sydney and move the camera
-    LatLng sydney = new LatLng(-34, 151);
-    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-  
     PostFetcher pf = new PostFetcher(this);
     pf.params()
-      .limit(100)
       .position(new LatLng(35.606120, 139.527240))
       .apply()
       .execute();
+  
     mClusterManager = new ClusterManager<>(this, mMap);
     mMap.setOnCameraIdleListener(mClusterManager);
     mMap.setOnMarkerClickListener(mClusterManager);
