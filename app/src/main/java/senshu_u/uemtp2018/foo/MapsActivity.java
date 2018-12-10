c8517d2ff195efe9a12168d6ee4865e3aa689cf1
S-zebra <shimashimazebra13@gmail.com>
2018-12-11 00:51:43 +0900

    clusterManagerの初期化をマップ準備完了時に変更 postsのnull(エラー)チェック追加

--- app/src/main/java/senshu_u/uemtp2018/foo/MapsActivity.java
+++ app/src/main/java/senshu_u/uemtp2018/foo/MapsActivity.java
@@ -3,6 +3,7 @@ package senshu_u.uemtp2018.foo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,28 +43,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   @Override
   public void onMapReady(GoogleMap googleMap) {
     mMap = googleMap;
-    
+  
     // Add a marker in Sydney and move the camera
     LatLng sydney = new LatLng(-34, 151);
     mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
     mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    
+  
     PostFetcher pf = new PostFetcher(this);
     pf.params()
       .limit(100)
       .position(new LatLng(35.606120, 139.527240))
       .apply()
       .execute();
+    mClusterManager = new ClusterManager<>(this, mMap);
+    mMap.setOnCameraIdleListener(mClusterManager);
+    mMap.setOnMarkerClickListener(mClusterManager);
   }
   
   @Override
   public void onPostsFetched(List<Post> posts) {
+    if (posts == null) {
+      Toast.makeText(this, "投稿を取得できませんでした。", Toast.LENGTH_SHORT).show();
+      return;
+    }
     Log.d(getClass().getSimpleName(), posts.toString());
-    mClusterManager = new ClusterManager<>(this, mMap);
-    mMap.setOnCameraIdleListener(mClusterManager);
-    mMap.setOnMarkerClickListener(mClusterManager);
     for (Post post : posts) {
-//      Log.d("MapsActivity", "Adding" + post.toString());
       mClusterManager.addItem(post);
     }
   }
