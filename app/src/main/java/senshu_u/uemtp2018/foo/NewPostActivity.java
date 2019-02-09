8960e83bc0b955928bb771835af09619a885a14d
S-zebra <shimashimazebra13@gmail.com>
2019-02-09 22:01:22 +0900

    再配置機能を実装

--- app/src/main/java/senshu_u/uemtp2018/foo/NewPostActivity.java
+++ app/src/main/java/senshu_u/uemtp2018/foo/NewPostActivity.java
@@ -2,10 +2,10 @@ package senshu_u.uemtp2018.foo;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,20 +26,24 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.io.IOException;
 import java.util.List;
 
 
 public class NewPostActivity extends AppCompatActivity implements PostSendCallback {
+  public static final String TAG = NewPostActivity.class.getSimpleName();
   public static final String PARENT_ID = "parentID";
   public static final String PARENT_TEXT = "parentText";
+  public static final int RELOCATE_REQ_CODE = 1;
   
   private FusedLocationProviderClient client;
   private TextView locationLabel;
+  private final MyLocationCallback locationCallback = new MyLocationCallback();
   private EditText contentEditor;
-  private MyLocationCallback locationCallback = new MyLocationCallback();
-  private Location lastLocation;
+  private Button relocateButton;
+  private LatLng lastLocation;
   private String token;
   private int parentID;
   
@@ -54,14 +59,46 @@ public class NewPostActivity extends AppCompatActivity implements PostSendCallba
     contentEditor = findViewById(R.id.contentEditor);
     setSupportActionBar(toolbar);
     startUpdatingLocation();
+  
     token = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE).getString(TsukumoAPI.TOKEN_KEY, null);
     parentID = getIntent().getIntExtra(PARENT_ID, -1);
     inReplyToHeader = findViewById(R.id.inReplyToHeader);
     inReplyToLabel = findViewById(R.id.replyToLabel);
+  
     if (parentID > 0) {
       inReplyToHeader.setVisibility(View.VISIBLE);
       inReplyToLabel.setVisibility(View.VISIBLE);
       inReplyToLabel.setText(getIntent().getStringExtra(PARENT_TEXT));
+    }
+  
+    relocateButton = findViewById(R.id.relocateButton);
+    relocateButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent i = new Intent(NewPostActivity.this, RelocateActivity.class);
+        if (lastLocation != null) {
+          i.putExtra(RelocateActivity.EXTRA_LOCATION, lastLocation);
+        }
+        startActivityForResult(i, RELOCATE_REQ_CODE);
+      }
+    });
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.new_post_menu, menu);
+    return true;
+  }
+  
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    Log.d(TAG, String.valueOf(data));
+    if (requestCode == RELOCATE_REQ_CODE) {
+      if (data == null) return;
+      client.removeLocationUpdates(locationCallback);
+      lastLocation = data.getParcelableExtra(RelocateActivity.EXTRA_LOCATION);
+      updateLocationDisplay();
     }
   }
   
@@ -91,12 +128,6 @@ public class NewPostActivity extends AppCompatActivity implements PostSendCallba
   }
   
   @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.new_post_menu, menu);
-    return true;
-  }
-  
-  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     if (item.getItemId() == R.id.send) {
       if (lastLocation == null) return true;
@@ -106,7 +137,7 @@ public class NewPostActivity extends AppCompatActivity implements PostSendCallba
       }
       String text = contentEditor.getText().toString();
       if (text.length() == 0) return true;
-      Post newPost = new Post(lastLocation.getLatitude(), lastLocation.getLongitude(), text);
+      Post newPost = new Post(lastLocation.latitude, lastLocation.longitude, text);
       if (parentID > 0) {
         newPost.setParentID(this.parentID);
       }
@@ -114,6 +145,20 @@ public class NewPostActivity extends AppCompatActivity implements PostSendCallba
       sender.execute(newPost);
     }
     return true;
+  }
+  
+  public void updateLocationDisplay() {
+    Geocoder gc = new Geocoder(NewPostActivity.this);
+    StringBuilder sb = new StringBuilder(String.format("%.5f %.5f", lastLocation.latitude, lastLocation.longitude) + "\n");
+    try {
+      List<Address> addresses = gc.getFromLocation(lastLocation.latitude, lastLocation.longitude, 1);
+      Log.d(getClass().getSimpleName(), "Addresses: " + addresses.toString());
+      Address firstAddress = addresses.get(0);
+      sb.append(firstAddress.getAddressLine(0));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    locationLabel.setText(sb.toString());
   }
   
   @Override
@@ -130,19 +175,8 @@ public class NewPostActivity extends AppCompatActivity implements PostSendCallba
   class MyLocationCallback extends LocationCallback {
     @Override
     public void onLocationResult(LocationResult result) {
-      lastLocation = result.getLastLocation();
-      Geocoder gc = new Geocoder(NewPostActivity.this);
-      StringBuilder sb = new StringBuilder(String.format("%.5f %.5f", lastLocation.getLatitude(), lastLocation.getLongitude()) + "\n");
-      try {
-        List<Address> addresses = gc.getFromLocation(lastLocation.getLatitude(), lastLocation.getLongitude(), 1);
-        Log.d(getClass().getSimpleName(), "Addresses: " + addresses.toString());
-        Address firstAddress = addresses.get(0);
-        sb.append(firstAddress.getAddressLine(0));
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      locationLabel.setText(sb.toString());
+      lastLocation = new LatLng(result.getLastLocation().getLatitude(), result.getLastLocation().getLongitude());
+      updateLocationDisplay();
     }
-    
   }
 }
